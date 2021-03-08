26e7f3bf93a731a5e4c57ecbd57fd2edd11df53a
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-03-08 14:49:06 +0100

    formatting

--- src/main/java/io/qiot/covid19/datahub/importer/domain/relational/RawTelemetryHistoryType.java
+++ src/main/java/io/qiot/covid19/datahub/importer/domain/relational/RawTelemetryHistoryType.java
@@ -6,25 +6,25 @@ package io.qiot.covid19.datahub.importer.domain.relational;
  * @author andreabattaglia
  */
 public enum RawTelemetryHistoryType {
-    
+
     /** The so 2. */
-    so2, 
- /** The temperature. */
- temperature, 
- /** The humidity. */
- humidity, 
- /** The dew. */
- dew, 
- /** The co. */
- co, 
- /** The o 3. */
- o3, 
- /** The pm 25. */
- pm25, 
- /** The no 2. */
- no2, 
- /** The pressure. */
- pressure, 
- /** The pm 10. */
- pm10
+    so2,
+    /** The temperature. */
+    temperature,
+    /** The humidity. */
+    humidity,
+    /** The dew. */
+    dew,
+    /** The co. */
+    co,
+    /** The o 3. */
+    o3,
+    /** The pm 25. */
+    pm25,
+    /** The no 2. */
+    no2,
+    /** The pressure. */
+    pressure,
+    /** The pm 10. */
+    pm10
 }
