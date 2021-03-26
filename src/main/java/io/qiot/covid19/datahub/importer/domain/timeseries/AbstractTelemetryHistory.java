87ecb54e78af0caf2e854451b1da09c9283d200f
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-03-26 16:15:09 +0100

    Removed timeseries dest; switched to native mode

--- src/main/java/io/qiot/covid19/datahub/importer/domain/timeseries/AbstractTelemetryHistory.java
+++ src/main/java/io/qiot/covid19/datahub/importer/domain/timeseries/AbstractTelemetryHistory.java
@@ -1,74 +1,74 @@
-package io.qiot.covid19.datahub.importer.domain.timeseries;
-
-import java.time.Instant;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.influxdb.annotations.Column;
-
-import io.qiot.covid19.datahub.importer.domain.relational.StationHistory;
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-/**
- * The Class AbstractTelemetryHistory.
- *
- * @author andreabattaglia
- */
-@RegisterForReflection
-public abstract class AbstractTelemetryHistory extends StationHistory {
-
-    /** The time. */
-    @JsonProperty(value = "instant")
-    @Column(timestamp = true)
-    public Instant time;
-
-    /**
-     * Hash code.
-     *
-     * @return the int
-     */
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((city == null) ? 0 : city.hashCode());
-        result = prime * result + ((country == null) ? 0 : country.hashCode());
-        result = prime * result + ((time == null) ? 0 : time.hashCode());
-        return result;
-    }
-
-    /**
-     * Equals.
-     *
-     * @param obj the obj
-     * @return true, if successful
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        AbstractTelemetryHistory other = (AbstractTelemetryHistory) obj;
-        if (city == null) {
-            if (other.city != null)
-                return false;
-        } else if (!city.equals(other.city))
-            return false;
-        if (country == null) {
-            if (other.country != null)
-                return false;
-        } else if (!country.equals(other.country))
-            return false;
-        if (time == null) {
-            if (other.time != null)
-                return false;
-        } else if (!time.equals(other.time))
-            return false;
-        return true;
-    }
-
-    
-
-}
+//package io.qiot.covid19.datahub.importer.domain.timeseries;
+//
+//import java.time.Instant;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.influxdb.annotations.Column;
+//
+//import io.qiot.covid19.datahub.importer.domain.relational.StationHistory;
+//import io.quarkus.runtime.annotations.RegisterForReflection;
+//
+///**
+// * The Class AbstractTelemetryHistory.
+// *
+// * @author andreabattaglia
+// */
+//@RegisterForReflection
+//public abstract class AbstractTelemetryHistory extends StationHistory {
+//
+//    /** The time. */
+//    @JsonProperty(value = "instant")
+//    @Column(timestamp = true)
+//    public Instant time;
+//
+//    /**
+//     * Hash code.
+//     *
+//     * @return the int
+//     */
+//    @Override
+//    public int hashCode() {
+//        final int prime = 31;
+//        int result = 1;
+//        result = prime * result + ((city == null) ? 0 : city.hashCode());
+//        result = prime * result + ((country == null) ? 0 : country.hashCode());
+//        result = prime * result + ((time == null) ? 0 : time.hashCode());
+//        return result;
+//    }
+//
+//    /**
+//     * Equals.
+//     *
+//     * @param obj the obj
+//     * @return true, if successful
+//     */
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj)
+//            return true;
+//        if (obj == null)
+//            return false;
+//        if (getClass() != obj.getClass())
+//            return false;
+//        AbstractTelemetryHistory other = (AbstractTelemetryHistory) obj;
+//        if (city == null) {
+//            if (other.city != null)
+//                return false;
+//        } else if (!city.equals(other.city))
+//            return false;
+//        if (country == null) {
+//            if (other.country != null)
+//                return false;
+//        } else if (!country.equals(other.country))
+//            return false;
+//        if (time == null) {
+//            if (other.time != null)
+//                return false;
+//        } else if (!time.equals(other.time))
+//            return false;
+//        return true;
+//    }
+//
+//    
+//
+//}
--- src/main/java/io/qiot/covid19/datahub/importer/domain/timeseries/GasTelemetryHistory.java
+++ src/main/java/io/qiot/covid19/datahub/importer/domain/timeseries/GasTelemetryHistory.java
@@ -1,74 +1,74 @@
-package io.qiot.covid19.datahub.importer.domain.timeseries;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.influxdb.annotations.Column;
-import com.influxdb.annotations.Measurement;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-/**
- * The Class GasTelemetryHistory.
- *
- * @author andreabattaglia
- */
-@RegisterForReflection
-@Measurement(name = "gashistory")
-//@Entity
-//@Table(name = "temporarygashistory")
-public class GasTelemetryHistory extends AbstractTelemetryHistory {
-
-    /** The adc. */
-    @JsonProperty(value = "adc")
-    @Column
-    public Double adc;
-    
-    /** The nh 3. */
-    @JsonProperty(value = "nh3")
-    @Column
-    public Double nh3;
-    
-    /** The oxidising. */
-    @JsonProperty(value = "oxidising")
-    @Column
-    public Double oxidising;
-    
-    /** The reducing. */
-    @JsonProperty(value = "reducing")
-    @Column
-    public Double reducing;
-
-    /**
-     * To string.
-     *
-     * @return the string
-     */
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("GasTelemetryHistory [time=");
-        builder.append(time);
-        builder.append(", city=");
-        builder.append(city);
-        builder.append(", country=");
-        builder.append(country);
+//package io.qiot.covid19.datahub.importer.domain.timeseries;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.influxdb.annotations.Column;
+//import com.influxdb.annotations.Measurement;
+//
+//import io.quarkus.runtime.annotations.RegisterForReflection;
+//
+///**
+// * The Class GasTelemetryHistory.
+// *
+// * @author andreabattaglia
+// */
+//@RegisterForReflection
+//@Measurement(name = "gashistory")
+////@Entity
+////@Table(name = "temporarygashistory")
+//public class GasTelemetryHistory extends AbstractTelemetryHistory {
+//
+//    /** The adc. */
+//    @JsonProperty(value = "adc")
+//    @Column
+//    public Double adc;
+//    
+//    /** The nh 3. */
+//    @JsonProperty(value = "nh3")
+//    @Column
+//    public Double nh3;
+//    
+//    /** The oxidising. */
+//    @JsonProperty(value = "oxidising")
+//    @Column
+//    public Double oxidising;
+//    
+//    /** The reducing. */
+//    @JsonProperty(value = "reducing")
+//    @Column
+//    public Double reducing;
+//
+//    /**
+//     * To string.
+//     *
+//     * @return the string
+//     */
+//    @Override
+//    public String toString() {
+//        StringBuilder builder = new StringBuilder();
+//        builder.append("GasTelemetryHistory [time=");
+//        builder.append(time);
+//        builder.append(", city=");
+//        builder.append(city);
+//        builder.append(", country=");
+//        builder.append(country);
+////        builder.append(", longitude=");
+////        builder.append(geometry.getX());
+////        builder.append(", latitude=");
+////        builder.append(geometry.getY());
 //        builder.append(", longitude=");
-//        builder.append(geometry.getX());
+//        builder.append(longitude);
 //        builder.append(", latitude=");
-//        builder.append(geometry.getY());
-        builder.append(", longitude=");
-        builder.append(longitude);
-        builder.append(", latitude=");
-        builder.append(latitude);
-        builder.append(", adc=");
-        builder.append(adc);
-        builder.append(", nh3=");
-        builder.append(nh3);
-        builder.append(", oxidising=");
-        builder.append(oxidising);
-        builder.append(", reducing=");
-        builder.append(reducing);
-        builder.append("]");
-        return builder.toString();
-    }
-
-}
+//        builder.append(latitude);
+//        builder.append(", adc=");
+//        builder.append(adc);
+//        builder.append(", nh3=");
+//        builder.append(nh3);
+//        builder.append(", oxidising=");
+//        builder.append(oxidising);
+//        builder.append(", reducing=");
+//        builder.append(reducing);
+//        builder.append("]");
+//        return builder.toString();
+//    }
+//
+//}
